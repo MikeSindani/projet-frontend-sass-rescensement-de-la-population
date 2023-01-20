97b82127527574ba94f76949b1eec725173e9af4
MikeSindani <mikems@live.fr>
2023-01-20 21:20:19 +0200

    finch to code index page and dashbord section home page

--- js/script-dashbord.js
+++ js/script-dashbord.js
@@ -24,7 +24,7 @@ function myFunction(x) {
   myFunction(x) // Call listener function at run time
   x.addListener(myFunction) // Attach listener function on state changes
 
-
+/*! code pour gerer les submenu du sidebar */ 
 const subBtn1 = document.querySelector(".sub-btn-1")
 const subMenu1 = document.querySelector(".sub-menu-1")
 const subBtn2 = document.querySelector(".sub-btn-2")
@@ -41,8 +41,81 @@ subBtn2.addEventListener("click",()=>{
 
 })
 /* fin de side bar menu hamberger */ 
+/* debut du code pour le message */ 
+const close_msg = document.querySelector(".close-msg")
+const msg = document.querySelector(".message")
+const header = document.querySelector("header")
+close_msg.addEventListener("click",()=>{
+   msg.style.display = "none"
+   header.classList.toggle("close-msg-header")
+})
+/* fin  du code pour le message */ 
+/* CODE DE CHART */
+const ctx = document.getElementById('Chart-by-Commune');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Katuba', 'Lubumbashi', 'Kambeba', 'Annexe', 'Rwashi', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+        backgroundColor: ['#3098FF']
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  const ctx1 = document.getElementById('Chart-by-humain');
+
+  new Chart(ctx1, {
+    type: 'pie',
+    data: {
+      labels: ['Homme', 'Femme'],
+      datasets: [{
+        label: 'Nombre des hommes  et des femmes',
+        data: [120000, 190000],
+        borderWidth: 1,
+        backgroundColor: ['#3098FF','#F8DB34']
+        
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 
 
 
+  const ctx3 = document.getElementById('Chart-by-humain-2');
 
-
+  new Chart(ctx3, {
+    type: 'bar',
+    data: {
+      labels: ['Homme', 'Femme'],
+      datasets: [{
+        data: [120000, 190000],
+        borderWidth: 1,
+        backgroundColor: ['#3098FF','#F8DB34']
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+ 
